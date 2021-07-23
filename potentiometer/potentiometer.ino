347f145973921fe5692d7fa597d9d4ac80f69fd7
eaxm <30189577+eaxm@users.noreply.github.com>
2021-07-23 15:58:27 +0200

    Connected oled display and added adafruit library

--- potentiometer/potentiometer.ino
+++ potentiometer/potentiometer.ino
@@ -4,6 +4,23 @@
  * Info about the protocol structure is in our first presentation
  */
 
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library.
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
 const byte startByte = 0xBE;
 const byte bufferLength = 2;
 
@@ -29,6 +46,15 @@ uint8_t gencrc(uint8_t *data, size_t len)
 
 void setup() {
   Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.display();
+
 }
 
 void loop() {
